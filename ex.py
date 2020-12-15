f7f9ff5265e0bc3fec5bf77bcb0c9f99f0c0837b
burmist <burm.leonid@gmail.com>
2020-12-15 17:55:55 +0100

    Add subplots

--- ex.py
+++ ex.py
@@ -51,7 +51,7 @@ def plot_scatter():
 
     fig = plt.figure(num=None, figsize=None, dpi=None, facecolor=None,
                      edgecolor=None, frameon=True, clear=False)
-    ax = fig.add_axes([0.1, 0.1, 0.85, 0.85])
+    fig.add_axes([0.1, 0.1, 0.85, 0.85])
     
     plt.scatter(x, y, s=None, c=None, marker=None, cmap=None, norm=None,
                 vmin=None, vmax=None, alpha=None, linewidths=None,
@@ -63,10 +63,50 @@ def plot_scatter():
     plt.xlabel('The x lable')
     plt.ylabel('The y lable')
     plt.show()
+
+@printinfo
+def plot_subplot01():
+
+    np.random.seed(19680801)
+    x1 = np.random.random(100)*5
+    y1 = np.random.random(100)*5
+    x2 = np.random.random(100)*5
+    y2 = np.random.random(100)*5
+
+    # using the variable axs for multiple Axes
+    fig, axs = plt.subplots( nrows=2, ncols=1,
+                             sharex=False, sharey=False,
+                             squeeze=True, subplot_kw=None,
+                             gridspec_kw=None,
+                             figsize=(10,12))
+    #plt.tight_layout()
+
+    # 
+    axs[0].plot(x1, y1)
+    axs[0].set_xbound(-0.5,5.5)
+    axs[0].set_ybound(-0.5,5.5)
+    axs[0].set_title('The title 0')
+    axs[0].set_xlabel('The x lable 0')
+    axs[0].set_ylabel('The y lable 0')
+    axs[0].set_position([0.05,0.55,0.9,0.40])
+    #
+    axs[1].scatter(x2, y2)
+    axs[1].set_xbound(-0.5,5.5)
+    axs[1].set_ybound(-0.5,5.5)
+    axs[1].set_title('The title 1')
+    axs[1].set_xlabel('The x lable 1')
+    axs[1].set_ylabel('The y lable 1')
+    axs[1].set_position([0.05,0.05,0.9,0.40])
+    #
+    print(type(axs))
+    print(type(axs[0]))
+    
+    plt.show()    
     
 def main():
     plot_errorbar()
     plot_scatter()
+    plot_subplot01()
     
 if __name__ == "__main__":
     main()
