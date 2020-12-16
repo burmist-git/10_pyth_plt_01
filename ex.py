25c1c52a2dfb71721f63f429d1d0f0bc1f05693f
burmist <burm.leonid@gmail.com>
2020-12-16 16:49:38 +0100

    Add histo examples

--- ex.py
+++ ex.py
@@ -102,11 +102,65 @@ def plot_subplot01():
     print(type(axs[0]))
     
     plt.show()    
+
+def getHistBins(nBins,xMin,xMax):
+    binW = (xMax - xMin)/nBins
+    return [xMin + binW*i for i in range(nBins+1)]
+
+def generateHistData(nn,x0,sigma):
+    return [sigma[i]*np.random.randn(nn[i], 1) + x0[i] for i in range(len(nn))]
+
+@printinfo
+def plot_hist():
+
+    classType = [0,1,2,3]
+    nn    = [2000,10000,5000,5000]
+    x0    = [1,2,4,5]
+    sigma = [0.3,0.3,0.8,0.2]
+
+    nPlots=len(nn) + 2
+    ncols=math.ceil(nPlots**(0.5))
+    nrows=int(nPlots/ncols)+math.ceil(nPlots/ncols-int(nPlots/ncols))
+    print(ncols)
+    print(nrows)
+    
+    nBins = 100
+    nSigma = 4
+    xMin = int(np.array(x0).min() - nSigma*np.array(sigma).max())
+    xMax = int(np.array(x0).max() + nSigma*np.array(sigma).max())
+    bins = getHistBins(nBins,xMin,xMax)
+    print(len(bins))
+    #print(xMin)
+    #print(xMax)
+    
+    data = generateHistData(nn,x0,sigma)
+    datay = [classType[i]*np.ones(nn[i]) for i in range(len(x0))]
+    
+    # using the variable axs for multiple Axes
+    fig, axs = plt.subplots( nrows=nrows, ncols=ncols,
+                             sharex=False, sharey=False,
+                             squeeze=True, subplot_kw=None,
+                             gridspec_kw=None,
+                             figsize=(18,8))
+    plt.tight_layout()
+
+    for i in range(nrows):
+        for j in range(ncols):
+            plotID=i*ncols + j
+            if(plotID<len(nn)):
+                axs[i,j].hist(data[plotID], bins=bins)
+            elif (plotID == len(nn)):
+                axs[i,j].hist(np.vstack(data), bins=bins)                
+            elif (plotID == (len(nn)+1)):
+                axs[i,j].scatter(np.vstack(data),np.hstack(datay))                
+    
+    plt.show()
     
 def main():
-    plot_errorbar()
-    plot_scatter()
-    plot_subplot01()
+    #plot_errorbar()
+    #plot_scatter()
+    #plot_subplot01()
+    plot_hist()
     
 if __name__ == "__main__":
     main()
